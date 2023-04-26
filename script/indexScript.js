c97495825066916526889ea21f064dd59a13408a
CamilleBainm <131908357+CamilleBainm@users.noreply.github.com>
2023-04-26 21:59:40 +0200

    Add files via upload

---
+++ script/indexScript.js
@@ -0,0 +1,12 @@
+document.getElementById("play").addEventListener("click", function() 
+{
+  window.location.href = "game.html";
+}
+);
+
+document.querySelector(".exit").addEventListener("click", function()
+{
+	window.location.replace("https://playsnake.org/");
+	window.close();//ne peux fermer que des pages ouverte par le script
+}
+);
