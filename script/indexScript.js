26b60c592fd2615879be9b7a89c720131c74a36d
Camille <bainmangin.camille@gmail.com>
2023-05-02 10:24:19 +0200

    Update indexScript.js

--- script/indexScript.js
+++ script/indexScript.js
@@ -9,4 +9,4 @@ document.querySelector(".exit").addEventListener("click", function()
 	window.location.replace("https://playsnake.org/");
 	window.close();//ne peux fermer que des pages ouverte par le script
 }
-);
+);//hello
